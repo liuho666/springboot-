060937aa0d14242922ce929c1d9f456a53d9fa70
liuhong <573296511@qq.com>
2020-11-23 00:13:23 +0800

    提交到本地仓库

---
+++ src/main/java/com/liu/zuanji/ZuanjiApplication.java
@@ -0,0 +1,13 @@
+package com.liu.zuanji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuanjiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuanjiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liu/zuanji/ZuanjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liu.zuanji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuanjiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
